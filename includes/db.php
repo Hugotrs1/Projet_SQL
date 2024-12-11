3173dd0703bd7c45781f0cf0491882c82ab3ff03
Hugo Troussel <163901405+Hugotrs1@users.noreply.github.com>
2024-12-11 09:34:12 +0100

    Create db.php

---
+++ includes/db.php
@@ -0,0 +1,8 @@
+<?php
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=gta_database;charset=utf8", "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
